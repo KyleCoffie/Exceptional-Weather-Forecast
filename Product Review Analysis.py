22a8d36321d0d7d80ec146f3838b513d9f6f76ff
KyleCoffie <160148467+KyleCoffie@users.noreply.github.com>
2024-08-08 12:58:50 -0500

    third commit

--- Product Review Analysis.py
+++ Product Review Analysis.py
@@ -70,10 +70,31 @@ reviews = [
         "Poor quality product. Wouldn't recommend it to anyone.",
         "The product was average. Nothing extraordinary about it."]
 
-summary = [review.split() for review in reviews]
-def summarized_review(summary):
-    if len(summary) >30:
-        return summary[:30] + "..."
-    return summary
+def summarize_reviews(reviews):
+  # We want to return an array because the original argument was an array 
+  reviews_to_return = [] 
 
-print (summary)
+# Iterate through reviews 
+  for review in reviews:
+
+    # If the review is less than 30 characters, add the original review to our reviews_to_return 
+    if len(review) <= 30:
+      reviews_to_return.append(review)
+
+    # If the reviews are longer than 30 characters, we want to do something to it 
+    else:
+      # Find the cutoff (where is the last space in our first 30 characters?)
+      # string.rfind(value we want to find)
+      cutoff = review[:30].rfind(' ')
+
+      # review[:cutoff] + ... 
+      if cutoff != -1:
+        reviews_to_return.append(review[:cutoff] + "...")
+      else:
+        reviews_to_return.append(review[:30] + "...")
+
+  return reviews_to_return
+        
+
+
+print(summarize_reviews(reviews))
